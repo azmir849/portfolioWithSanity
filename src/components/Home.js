b349c459b48872fb323122237174e9f180d6605f
azmir849 <azmir15-849@diu.edu.bd>
2022-01-27 05:15:50 +0600

    Navbar and home page updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,65 @@
 import React from 'react'
-import bg from '../img/bg.jpg'
 export default function Home() {
     return (
-       <main>
-           <img
-            alt='Ah'
-            className='absolute object-cover w-full h-full'
-             src={bg} />
-          
-           <section className='relative flex justify-center min-h-screen pt-12  lg:pt-64 px-8'>
-               <h1 className='text-6xl text-white hundred font-bold cursive leading-none lg:leading-snug home-name '>Hello. I'm AH Naeem</h1>
-           </section>
-       </main>
+    <div id="home" class="header">
+		<div class="container">
+				<div class="banner-info">
+					<div class="col-md-7 header-right">
+						<h1>Hi !</h1>
+						<h6>I am a</h6>
+						<h6>Front-End Developer</h6>
+						{/* <p className='text-muted'>(MongoDB, Express Js, React Js, Node Js)</p> */}
+						<ul class="address">
+
+							<li>
+								<ul class="address-text">
+									<li><b>NAME</b></li>
+									<li>MD. Azmir Hossen</li>
+								</ul>
+							</li>
+							<li>
+								<ul class="address-text">
+									<li><b>PHONE </b></li>
+									<li>+88 01627-297942</li>
+								</ul>
+							</li>
+							<li>
+								<ul class="address-text">
+									<li><b>ADDRESS </b></li>
+									<li>H-39,Road-17A,Sector-12,Uttara,Dhaka-1230</li>
+								</ul>
+							</li>
+							<li>
+								<ul class="address-text">
+									<li><b>E-MAIL </b></li>
+									<li><a href="mailto:azmir15-849@diu.edu.bd"> azmir15-849@diu.edu.bd</a></li>
+								</ul>
+							</li>
+							<li>
+								<ul class="address-text">
+									<li><b>LINKEDIN</b></li>
+									<li><a
+											href="https://www.linkedin.com/in/ah-naeem/">https://www.linkedin.com/in/ah-naeem/</a>
+									</li>
+								</ul>
+							</li>
+							<li>
+								<ul class="address-text">
+									<li><b>GITHUB</b></li>
+									<li><a href="https://github.com/azmir849">https://github.com/azmir849</a></li>
+								</ul>
+							</li>
+
+						</ul>
+					</div>
+					<div class="flex-column col-md-5 header-left d-flex justify-content-center">
+						<a href='/'><button type="button" class="btn btn-warning btn-lg">Download
+								Resume</button></a>
+					</div>
+					<div class="clearfix"> </div>
+
+				</div>
+			</div>
+		</div>
     )
 }
