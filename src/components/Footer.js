a933a6a0ce77725d6dde6ef168d90e5696a1b3b8
azmir849 <azmir15-849@diu.edu.bd>
2022-01-30 02:04:40 +0600

    footer fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return (
         <footer className="mt-5 mb-3" >
-            <p className="text-center"><span className="text-muted"> © Copyright {new Date().getUTCFullYear()} MD.Azmir Hoosen (AH Naeem). All rights reserved.</span></p>
+            <p className="text-center"><span className="text-muted"> © Copyright {new Date().getUTCFullYear()} MD.Azmir Hossen (AH Naeem). All rights reserved.</span></p>
         </footer>
 
 
